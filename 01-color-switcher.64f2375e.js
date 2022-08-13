6b92147092af6f6005aeeec26599254de7a15e66
TanyaP2022 <96392341+TanyaP2022@users.noreply.github.com>
2022-08-13 16:11:57 +0000

    Deploying to gh-pages from @ TanyaP2022/goit-js-hw-09@acb4c9a0e9ce402976ef478457476023c9e122df 🚀

---
+++ 01-color-switcher.64f2375e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");document.querySelector("body");e.setAttribute("disabled","true"),t.addEventListener("click",(function(){r=setInterval(d,1e3,1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled","true")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled","true"),e.setAttribute("disabled","true")}));let r=null;function d(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}
+//# sourceMappingURL=01-color-switcher.64f2375e.js.map
