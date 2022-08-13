7b665d547f05f09d7a61f3214c56556e347b4211
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-13 17:28:43 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+const CHANGE_DELAY = 1000;
+
+startBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let intervalId = null;
+
+function onStartClick() {
+  intervalId = setInterval(getRandomHexColor, CHANGE_DELAY);
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled', 'true');
+}
+function onStopClick() {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled', 'true');
+  stopBtn.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
