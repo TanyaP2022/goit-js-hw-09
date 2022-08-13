d668d167b3a0d52f86dad8071b15f9438da92cf5
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-13 19:14:28 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -23,5 +23,7 @@ function onStopClick() {
 }
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return (body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`);
 }
